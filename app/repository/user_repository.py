765815c217ab1f642966d57fcf6c3b4d0b5413fa
Silue Namogoh <namogoh.silue@st2i.net>
2024-07-23 13:58:12 +0000

    ajout du repository de user

---
+++ app/repository/user_repository.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timezone
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.configurations.database import get_db
+from app.models.data.user import User
+from app.models.data.otp import OTP
+from app.models.request.user import UserBase
+
+
+class UserRepository:
+    def __init__(self, db: Session = Depends(get_db)):
+        self.db = db
+ 
+ 
+    def create_user(self, user: UserBase):
+        db_user = User(**user.dict())
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return db_user
+    
+    def update_user(self, user: UserBase):
+        pass
+
+    def delete_user(self, user_id: int):
+        pass
+
+    def get_all_users(self):
+        return self.db.query(User).all()
+
+       
+    def get_user_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
+    
+    def get_user_by_id(self, user_id: int):
+        return self.db.query(User).filter(User.id == user_id).first()
+    
+    def reset_password(self, email: str, new_password: str) -> User:
+        user = self.get_user_by_email(email)
+        if not user:
+            return None
+
+        otp_entry = self.db.query(OTP).filter(OTP.user_id == user.id, OTP.used == True, OTP.expires_at >= datetime.now(timezone.utc)).first()
+        
+        if not otp_entry:
+            return None
+
+        user.password = new_password
+        self.db.add(user)  
+        self.db.commit()
+
+        return user
+
+    def update_password(self, user: User, new_password: str) -> User:
+        user.password = new_password
+        self.db.commit()
+        
+    
