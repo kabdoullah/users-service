4fa9cac1a1e4e21d5a2b3ef0053f6c42e8b324b5
Kemogoha Coulibaly <kemogoha.coulibaly@st2i.net>
2024-07-24 10:24:24 +0000

    feat: nombre de tentative de connexion

--- app/api/login.py
+++ app/api/login.py
@@ -2,17 +2,16 @@ from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from app.exceptions.custom_exception import InvalidCredentialsException, InactiveUserException
 from app.security.security import ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_MINUTES, create_access_token, create_refresh_token
 from app.services.user_service import UserService
 from app.services.session_service import SessionService
-from app.models.requests.session import SessionResponse, SessionCreate
+from app.models.requests.session import LoginErrorResponse, LoginSuccessResponse, SessionCreate
 
 
 
 router = APIRouter()
 
-@router.post("/login", response_model=SessionResponse)
+@router.post("/login")
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],  user_service: UserService = Depends(UserService), session_service: SessionService = Depends(SessionService)):
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     refresh_token_expires = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
@@ -20,7 +19,7 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],  user_serv
     try:
         result = user_service.authenticate_user(email=form_data.username, password=form_data.password)
         if "user" not in result:
-            return result
+            return LoginErrorResponse(message=result["message"], attempts_left=result["attempts_left"]) 
         
         user = result["user"]
         
@@ -29,9 +28,16 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],  user_serv
         
         session_service.create_session(session_data=SessionCreate(user_id=user.id, token=access_token,created_at=datetime.now(timezone.utc), expires_at=access_token_expires))
         
-        return {"message": "Login successful", "access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+        return LoginSuccessResponse(
+            message="Login successful",
+            access_token=access_token,
+            refresh_token=refresh_token, 
+            attempts_left=result["attempts_left"]
+        )
     except HTTPException as e:
         raise e
+    
+    
     
 
 
--- app/models/requests/session.py
+++ app/models/requests/session.py
@@ -8,14 +8,20 @@ class SessionCreate(BaseModel):
     expired_at: Optional[datetime] = None
     created_at: Optional[datetime] = None
 
-class SessionResponse(BaseModel):
-    acces_token: str
-    refresh_token: str
-    token_type: str = "bearer"
+class LoginSuccessResponse(BaseModel):
+    message: str
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
+    attempts_left: int
     
-
     class Config:
         from_attributes = True
+
+class LoginErrorResponse(BaseModel):
+    message: str
+    attempts_left: int
+    
+
         
 class SessionData(BaseModel):
     user_id: UUID4
